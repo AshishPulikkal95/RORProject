cc10a1d43c0175bf5e6ff615f92746fdc6cc3579
AshishPulikkal95 <Apulikkal1995@gmail.com>
2018-10-02 19:32:15 +0000

    Create article table model and validations

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+    validates :title, presence: true, length: {minimum: 5, maximum: 30} #ensure that the article has some title and it is not null along with a length validation to ensure its not useless title.
+    validates :description, presence: true, length: {minimum: 5, maximum: 150} #ensure that the description has some title and it is not null. Length validations ensures description is not useless.
+end
