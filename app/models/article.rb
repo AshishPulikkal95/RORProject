3c3c22921e2a78ea025191d2dbbadf7b7ec003b7
AshishPulikkal95 <Apulikkal1995@gmail.com>
2018-11-02 22:57:14 +0000

    Update

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
     belongs_to :user
     has_many :article_categories
     has_many :categories, through: :article_categories
-    validates :title, presence: true, length: {minimum: 5, maximum: 30} #ensure that the article has some title and it is not null along with a length validation to ensure its not useless title.
-    validates :description, presence: true, length: {minimum: 5, maximum: 1000} #ensure that the description has some title and it is not null. Length validations ensures description is not useless.
+    validates :title, presence: true, length: {minimum: 5, maximum: 200} #ensure that the article has some title and it is not null along with a length validation to ensure its not useless title.
+    validates :description, presence: true, length: {minimum: 5, maximum: 10000} #ensure that the description has some title and it is not null. Length validations ensures description is not useless.
     validates :user_id, presence: true
 end
